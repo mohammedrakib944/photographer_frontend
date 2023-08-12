8d3c6f31a43aae91dcf236da45404b3731e7df2a
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-08-12 18:34:15 +0600

    Delete profile

--- src/components/common/PrintProfileImage.jsx
+++ src/components/common/PrintProfileImage.jsx
@@ -1,7 +1,5 @@
 "use client";
 import { API_URL } from "@/secret";
-import Image from "next/image";
-import DummyProfile from "@/utils/images/profile.svg";
 import { useGetProfileImagesQuery } from "@/redux/features/profileImage";
 
 //Nothing changes
@@ -11,18 +9,10 @@ const PrintProfileImage = () => {
 
   return (
     <>
-      {profileImage ? (
+      {profileImage && (
         <img
           className="max-w-[400px] rounded-full border-4 border-white hover:rotate-2 hover:scale-105 duration-150"
           src={`${API_URL}/images/profile/${profileImage}`}
-          width="100%"
-          height="100%"
-          alt="Image"
-        />
-      ) : (
-        <Image
-          className="max-w-[400px] rounded-full border-4 border-white hover:rotate-2 hover:scale-105 duration-150"
-          src={DummyProfile}
           width="100%"
           height="100%"
           alt="Image"
